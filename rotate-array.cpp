4e657f0e87118685f31994d50c9b0aa56101c05a
fanfank <reetsee@qq.com>
2015-04-09 22:37:33 +0800

    Add de Morgan method to rotate-array.cpp

--- rotate-array.cpp
+++ rotate-array.cpp
@@ -1,4 +1,4 @@
-//Solution 1: 就地移位，应该算是简单的做法
+//Solution 1: 就地移位
 //            In place rotating
 //            time: O(n)
 //            space: O(1)
@@ -25,6 +25,32 @@ public:
             nums[next] = tmp;
             ++cnt;
             ++st;
+        }
+    }
+};
+
+//Solution 2: 德摩根法
+//            de Morgan
+//            time: O(n)
+//            space: O(1)
+//            29ms
+class Solution {
+public:
+    void rotate(int nums[], int n, int k) {
+        if (n == 0 || k % n == 0) {
+            return;
+        }
+
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    void reverse(int nums[], int st, int ed) {
+        for (int i = st; i <= (st + ed) / 2; ++i) {
+            swap(nums[i], nums[ed-i+st]);
         }
     }
 };
